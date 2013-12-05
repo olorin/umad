c26708cfc9c1cc69fc25dd6243e8e8a18e2c0ef4
Barney Desmond <barney.desmond@anchor.net.au>
2013-12-05 17:25:54 +1100

    Grab the title of the page when indexing a generic HTTP request

--- distil/http_generic.py
+++ distil/http_generic.py
@@ -2,6 +2,12 @@ import sys
 # Fuck urllib2, that's effort. You may need to `pip install requests`.
 # http://docs.python-requests.org/en/latest/index.html
 import requests
+# lxml to parse the HTML and grab the title
+from lxml import html
+
+# Plaintext-ify all the junk we get
+import html2text
+
 
 def tidy_url(url):
 	"XXX: Not implemented yet"
@@ -13,6 +19,16 @@ def fetch(url):
 
 def blobify(url):
 	response = fetch(url)
-	return [{ 'url':url, 'blob':response.content }]
+
+	content = html2text.html2text(response.text)
+	doc_tree = html.fromstring(response.text)
+
+	title_list = doc_tree.xpath('//title/text()')
+	if title_list:
+		title = title_list[0]
+	else:
+		title = url
+
+	return [{ 'url':url, 'blob':content, 'title':title }]
 
 
