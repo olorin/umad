caeec29bc724a4ac784f93d88937e8fa063782c4
Barney Desmond <barney.desmond@anchor.net.au>
2013-12-12 18:56:26 +1100

    Add some real indexing code to the indexing_worker now

--- indexing_worker.py
+++ indexing_worker.py
@@ -3,6 +3,9 @@ import os
 
 import redis
 
+import distil
+from elasticsearch_backend import *
+
 DEBUG = os.environ.get('UMAD_INDEXING_WORKER_DEBUG')
 DEBUG = True
 PID_PREFIX = '[pid {0}] '.format(os.getpid())
@@ -14,15 +17,35 @@ def debug(msg, force_debug=False):
 		sys.stderr.flush()
 
 
+def index(url):
+	debug("-" * len("URL: %s" % url))
+	debug("URL: %s" % url)
+	debug("-" * len("URL: %s" % url))
+
+	try:
+		d = distil.Distiller(url)
+	except distil.NoUrlHandler:
+		print "Don't know how to handle URL: %s" % url
+		raise
+
+	for doc in d.docs:
+		if doc is None:
+			return
+		debug("Adding to index: %(url)s" % doc)
+		print doc['blob'][:400]
+		add_to_index(doc['url'], doc)
+		debug("Success!")
+		debug("")
+
+
+
 
 def main(argv=None):
 	debug("Debug logging is enabled")
 
 	teh_redis = redis.Redis(host='localhost', port=6379, db=0)
 
-
 	while True:
-		debug("About to loop")
 		try:
 			# Get URLs out of Redis. We're using this idiom to provide what is
 			# effectively a "BSPOP" (blocking pop from a set).
@@ -31,12 +54,11 @@ def main(argv=None):
 				url = teh_redis.spop('umad_indexing_queue')
 				if url is None:
 					break
-				debug("Processing url %s" % url)
-				pass # do something here
+				index(url)
 
-			debug("about to barber")
+			debug("The barber is napping")
 			teh_redis.brpop('barber')
-			debug("woke up")
+			debug("The barber was woken up!")
 		except Exception as e:
 			debug("Something went boom: {0}".format(e))
 
