52e02145ddf6b3c4d3021ecaf1b4b38676873ee9
Barney Desmond <barney.desmond@anchor.net.au>
2013-12-31 17:02:23 +1100

    nothing UNTYPED in the indices yet

--- localconfig.py
+++ localconfig.py
@@ -4,10 +4,7 @@
 # Make sure to add your new doc_types to this list as well as to
 # `determine_doc_type` below.
 KNOWN_DOC_TYPES = [ 'map', 'rt', 'provsys' ]
-KNOWN_DOC_TYPES.append('UNTYPED')
-
-# temp hack because map isn't indexed yet
-#KNOWN_DOC_TYPES = [ 'rt', 'provsys' ]
+#KNOWN_DOC_TYPES.append('UNTYPED')
 
 
 # Define this function yourself, remember to add doc_types to the list above.
