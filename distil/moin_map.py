4d361016e7b43080ee3d04a7b7b4e87ebe11fbca
Barney Desmond <barney.desmond@anchor.net.au>
2013-12-31 18:52:51 +1100

    Pretty much rewrite the whole moin map distiller

--- distil/moin_map.py
+++ distil/moin_map.py
@@ -1,9 +1,16 @@
-import sys
 import os
+import re
 # Fuck urllib2, that's effort. You may need to `pip install requests`.
 # http://docs.python-requests.org/en/latest/index.html
 import requests
 
+
+TITLE_RE    = re.compile(r'<<Title(\((.*)\))?>>', re.I)
+WIKIWORD_RE = re.compile(r'([a-z]+)([A-Z])')
+
+class FailedToRetrievePage(Exception):
+	def __init__(self, msg):
+		self.msg = msg
 
 def tidy_url(url):
 	"This is a hack that destroys query parameters"
@@ -17,18 +24,73 @@ def blobify(url):
 
 	url = tidy_url(url)
 
-	print "Going to get URL: {0}".format(url)
 	response = requests.get(url, auth=(MAPWIKI_USER, MAPWIKI_PASS), params={'action':'raw'}, verify='AnchorCA.pem')
+	try:
+		response.raise_for_status()
+	except:
+		raise FailedToRetrievePage("Error getting page from map wiki, got HTTP response %s" % response.status_code)
+
+	# An example URL:  https://map.engineroom.anchor.net.au/PoP/SYD1/NetworkPorts
+	#
+	# url      = https://map.engineroom.anchor.net.au/PoP/SYD1/NetworkPorts
+	# local_id = PoP/SYD1/NetworkPorts
+	# map      = PoP/SYD1/NetworkPorts
+	# title    = syd1 Network Port Allocations
+
+	page_lines = [ line.strip() for line in response.content.split('\n') ]
+
+	# Discard all lines beginning with one of: (keep line if all checks are not-hit)
+	#  - Comment (#)
+	#  - Macro (<<)
+	#  - Table tags (||)
+	page_lines = [ line for line in page_lines if  all( [ not line.startswith(x) for x in ['#', '||', '<<'] ] )  ]
+
+	# Content is now considered tidy
+	blob = '\n'.join(page_lines)
+
+	# Get the page name as according to Moin
+	page_name = url.replace('https://map.engineroom.anchor.net.au/', '')
+	if page_name == '': # Special case for the home page
+		page_name = 'ClueStick'
+
+
+	# Try to find a suitable title
+	lines_starting_with_title_macro = [ line for line in page_lines if line.startswith('<<Title') ]
+	lines_starting_with_equals_sign = [ line for line in page_lines if line.startswith('=') ]
+
+	title = None
+	if lines_starting_with_title_macro:
+		title_line = lines_starting_with_title_macro[0]
+		title = TITLE_RE.sub(r'\2', title_line).strip('"\'')
+	elif lines_starting_with_equals_sign:
+		title_line = lines_starting_with_equals_sign[0]
+		title = title_line.strip('= ')
+
+	if not title: # Either got left with an empty string, or still None
+		path_components = page_name.split('/')
+		title = ' '.join([ WIKIWORD_RE.sub(r'\1 \2', x) for x in path_components ])
+
+	# Try and find an exciting excerpt, this is complete and utter guesswork
+	indices_of_lines_beginning_with_equals_sign = [ i for i,x in enumerate(page_lines) if x.startswith('=') ]
+
+	if len(indices_of_lines_beginning_with_equals_sign) >= 2:
+		start,end = indices_of_lines_beginning_with_equals_sign[0:2]
+	elif len(indices_of_lines_beginning_with_equals_sign) == 1:
+		start = indices_of_lines_beginning_with_equals_sign[0]
+		end   = start + 5 # magic number
+	else:
+		start,end = 0,-1
+
+	excerpt = '\n'.join(page_lines[start+1:end]) # fencepost, not interested in the header line
+
 
 	document = {}
 	document['url']  = url
-	document['blob'] = response.content
-
-	# title
-	# excerpt
-	# last_updated
-	# local_id = URL with the stem stripped off
-	# title
+	document['blob'] = blob
+	document['local_id'] = page_name
+	document['map']      = page_name
+	document['title']    = title
+	document['excerpt']  = excerpt
 
 	yield document
 
