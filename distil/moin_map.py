ac7ecf7dc52cec25a2422907b079189fde469071
Barney Desmond <barney.desmond@anchor.net.au>
2014-01-02 12:18:25 +1100

    Change the way we generate excerpts, trying to deal with pages in the wild

--- distil/moin_map.py
+++ distil/moin_map.py
@@ -5,8 +5,9 @@ import re
 import requests
 
 
-TITLE_RE    = re.compile(r'<<Title(\((.*)\))?>>', re.I)
-WIKIWORD_RE = re.compile(r'([a-z]+)([A-Z])')
+TITLE_RE           = re.compile(r'<<Title(\((.*)\))?>>')
+NON_TITLE_MACRO_RE = re.compile(r'<<(?!Title[(>])')
+WIKIWORD_RE        = re.compile(r'([a-z]+)([A-Z])')
 
 class FailedToRetrievePage(Exception):
 	def __init__(self, msg):
@@ -37,21 +38,18 @@ def blobify(url):
 	# map      = PoP/SYD1/NetworkPorts
 	# title    = syd1 Network Port Allocations
 
-	page_lines = [ line.strip() for line in response.content.split('\n') ]
-
-	# Discard all lines beginning with one of: (keep line if all checks are not-hit)
-	#  - Comment (#)
-	#  - Macro (<<)
-	#  - Table tags (||)
-	page_lines = [ line for line in page_lines if  all( [ not line.startswith(x) for x in ['#', '||', '<<'] ] )  ]
-
-	# Content is now considered tidy
-	blob = '\n'.join(page_lines)
-
 	# Get the page name as according to Moin
 	page_name = url.replace('https://map.engineroom.anchor.net.au/', '')
 	if page_name == '': # Special case for the home page
 		page_name = 'ClueStick'
+
+	# Get the content
+	page_lines = [ line.strip() for line in response.content.split('\n') ]
+
+	# Discard all lines beginning with one of: (keep line if all checks are not-hit)
+	#  - Comment (#)
+	#  - Table tags (||)
+	page_lines = [ line for line in page_lines if  all( [ not line.startswith(x) for x in ['#', '||'] ] )  ]
 
 
 	# Try to find a suitable title
@@ -61,7 +59,11 @@ def blobify(url):
 	title = None
 	if lines_starting_with_title_macro:
 		title_line = lines_starting_with_title_macro[0]
-		title = TITLE_RE.sub(r'\2', title_line).strip('"\'')
+		title_match = TITLE_RE.match(title_line)
+		if not title_match.group(2):
+			title=None
+		else:
+			title = TITLE_RE.sub(r'\2', title_line).strip('"\'')
 	elif lines_starting_with_equals_sign:
 		title_line = lines_starting_with_equals_sign[0]
 		title = title_line.strip('= ')
@@ -71,8 +73,14 @@ def blobify(url):
 		title = ' '.join([ WIKIWORD_RE.sub(r'\1 \2', x) for x in path_components ])
 
 
+	# Strip all lines that are just macros
+	page_lines = [ line for line in page_lines if not NON_TITLE_MACRO_RE.match(line) ]
+
+	# Content is now considered tidy
+	blob = '\n'.join(page_lines)
+
 	# Try and find an exciting excerpt, this is complete and utter guesswork
-	indices_of_lines_beginning_with_equals_sign = [ i for i,x in enumerate(page_lines) if x.startswith('=') ]
+	indices_of_lines_beginning_with_equals_sign = [ i for i,x in enumerate(page_lines) if x.startswith('=') or x.startswith('<<Title') ]
 
 	if len(indices_of_lines_beginning_with_equals_sign) >= 2:
 		start,end = indices_of_lines_beginning_with_equals_sign[0:2]
