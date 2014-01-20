889f7c87fdebf5ad19e12af6aadc7cec2ed21488
Barney Desmond <barney.desmond@anchor.net.au>
2014-01-20 11:22:51 +1100

    Add index-add code and introduce some sanity-checking

--- elasticsearch_backend.py
+++ elasticsearch_backend.py
@@ -8,28 +8,24 @@ from localconfig import *
 
 es = elasticsearch.Elasticsearch(ELASTICSEARCH_NODES)
 
+class InvalidDocument(Exception): pass
 
-# XXX: This should be cleaned up to take a single argument, the 'value'.
+
+# XXX: This should be cleaned up to take a single argument, the 'document'.
 # In all cases, we're called as:
 #     add_to_index(doc['url'], doc)
 # We should pull out the URL ourselves.
-def add_to_index(key, value):
+def add_to_index(key, document):
 	doc_type = determine_doc_type(key)
 	index_name = "umad_%s" % doc_type
 
-	# Jam it into a data structure if we're dealing with a bare string
-	if type(value) == type('foo'):
-		# I'd like to kill this code, log to stderr if we hit this code
-		# path and see if we ever hit it. Nuke when it looks safe.
-		# -BD@2013-12-18
-		import sys
-		sys.stderr.write("We got a document that's a string instead "\
-				"of a data structure, make it go away.\nThe "\
-				"URL is: {0}\n".format(key).encode('utf8') )
-		sys.stderr.flush()
-		document = { "blob": value }
-	else:
-		document = value
+	# Sanity check the document. Our minimal requirement for the document
+	# is that it has a 'blob' and 'url' key, but ES will support much
+	# richer arbitrary fields.
+	if 'url' not in document:
+		raise InvalidDocument("The document MUST have a 'url' field, cannot add to index: {0}".format(document))
+	if 'blob' not in document:
+		raise InvalidDocument("The document MUST have a 'blob' field, cannot add to index: {0}".format(document))
 
 	# Making use of the local_id hack that shortcuts a way to searching for
 	# the "primary key" of a given doc_type
@@ -39,7 +35,6 @@ def add_to_index(key, value):
 	# Get the current time in UTC and set `last_indexed` on the document
 	document['last_indexed'] = datetime.datetime.now(tzutc())
 
-	# Minimal requirement for the document is that it has a 'blob' key, but ES will support much richer arbitrary schemas
 	es.index(
 		index = index_name,
 		doc_type = doc_type,
